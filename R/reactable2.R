a11ca7be0e103f87456688f72900c697a49cfcb2
Yilong Zhang <elong0527@gmail.com>
2023-12-03 21:30:02 +0000

    version 0.3.0

--- R/reactable2.R
+++ R/reactable2.R
@@ -60,3 +60,35 @@ reactable2 <- function(data,
     tbl
   }
 }
+
+#' Convert reactable to a data frame
+#'
+#' @param x  A `reactable` HTML widget
+#'
+#' @return A data frame
+#'
+reactable_to_df <- function(x){
+
+  # table data
+  tbl1 <- do.call(cbind, jsonlite::fromJSON(x$x$tag$attribs$data))
+
+  # table columns
+  columns <- x$x$tag$attribs$columns
+  tbl2 <- list()
+  for(i in seq_along(columns)){
+    if(! columns[[i]]$id %in% ".details"){
+      if(is.null(columns[[i]]$show)){
+        tbl2[[i]] <- unlist(columns[[i]][c("id", "name")])
+      }
+    }
+  }
+  tbl2 <- data.frame(do.call(rbind, tbl2))
+
+  # output
+  tbl <- data.frame(tbl1[, tbl2[["id"]]])
+
+  tbl[, 1] <- gsub("\U2000", " ", tbl[, 1])
+  attr(tbl, "column_header") <- paste(tbl2$name, collapse = "|")
+
+  tbl
+}
